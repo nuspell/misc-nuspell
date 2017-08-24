188e84d3d11bc60cc65f2a7c6bb8e63e5c9d9046
Dimitrij Mijoski <dmjpp@hotmail.com>
2017-08-24 22:31:27 +0200

    Update plan note, improve SoW generation.

--- sow.py
+++ sow.py
@@ -18,7 +18,8 @@ process_node(e)
 print()
 
 i = 1
-total = int(e.find('task-duration').text)
+total_duration = int(e.find('task-duration').text)
+total_cost = float(e.find('project-budget').text)
 
 for e1 in e.findall('./task-list/task'):
 
@@ -27,12 +28,20 @@ for e1 in e.findall('./task-list/task'):
         continue
 
     duration = int(e1.find('task-duration').text)
-    percent = duration / total
+    days = int(round(duration / 3600000 / 8))
+    percent = duration / total_duration
+    cost = int(round(percent * total_cost))
 
-    print('1.  {} ({:.2%})\n\n    '.format(name[2:],percent),end='')
+    print('{}.  {} (${}, {} days)\n\n    '.
+            format(i, name[2:], cost, days), end='')
     process_node(e1)
+    print()
     i += 1
 
+    j = 1
     for e2 in e1.findall('./task-list/task'):
-        print('\t1. ', end='')
+        print('\t{}. '.format(j), end='')
         process_node(e2)
+        j += 1
+
+    print()
