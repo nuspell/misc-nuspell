5d0ce8488d74de2e6d581f7623a553c65971df01
PanderMusubi <pander@users.sourceforge.net>
2017-11-06 12:56:48 +0100

    generate only new or newer plantuml

--- uml/generate.sh
+++ uml/generate.sh
@@ -4,11 +4,25 @@ function diagrams() {
 	type=`echo $1|sed -e 's/\(.*\)/\L\1/'`
 	if [ `find plantuml -name '*-'$type'-diagram.pu'|wc -l` -ne 0 ]; then
 		if [ -e $type-diagrams ]; then
-			rm -f $type-diagrams/*.svg
+			# Delete SVG files which have no PlantUML source file
+			cd $type-diagrams
+			for i in *.svg; do
+				source=`basename $i svg`pu
+				if [ `find ../plantuml -name $source|wc -l` -eq 0 ]; then
+					rm -f $i
+				fi
+			done
+			cd ..
 		else
 			mkdir $type-diagrams
 		fi
-		plantuml -tsvg -o ../$type-diagrams plantuml/*-$type-diagram.pu
+		# Only generate if PlantUML file is new or is newer than SVG file
+		for i in plantuml/*-$type-diagram.pu; do
+			svg=$type-diagrams/`basename $i pu`svg
+			if [ $i -nt $svg ]; then
+				plantuml -tsvg -o ../$type-diagrams $i
+			fi
+		done 
 		echo '* [Hunspell - UML '$1' Diagrams]('$type'-diagrams/README.md)' >> README.md
 		cd $type-diagrams
 		echo '# Hunspell - UML '$1' Diagrams' > README.md
