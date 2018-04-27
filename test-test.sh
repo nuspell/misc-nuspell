709153cf3264f97364a2e37ed5e26b9eb74e795d
PanderMusubi <pander@users.sourceforge.net>
2018-04-27 16:42:30 +0200

    add test test script

---
+++ test-test.sh
@@ -0,0 +1,11 @@
+cd ../1-support
+./1-download.sh
+./2-extract.sh
+./3-report-and-convert.sh
+cd ../2-word-lists
+./1-download.sh
+./2-extract.sh
+./3-report-and-convert.sh
+cd ../3-checks
+./1-prepare.sh
+./2-reference.sh
