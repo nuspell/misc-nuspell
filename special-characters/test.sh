bb68319f603975dcfb7035652800ae996523a29a
PanderMusubi <pander@users.sourceforge.net>
2018-05-15 12:45:35 +0200

    added punctuation and refactored

--- special-characters/test.sh
+++ special-characters/test.sh
@@ -4,9 +4,9 @@ for file in *aff; do
 	base=`basename $file .aff`
 	if [ $file != none.aff ]; then
 
-		hunspell -i UTF-8 -d none -a $base 2> /dev/null | grep -v 'International Ispell Version' | grep -v '^$' > $base-without-support.txt
+		hunspell -i UTF-8 -d none -a $base.words 2> /dev/null | grep -v 'International Ispell Version' | grep -v '^$' > $base-without-support.txt
 
-		hunspell -i UTF-8 -d $base -a $base 2> /dev/null | grep -v 'International Ispell Version' | grep -v '^$' > $base-with-support.txt
+		hunspell -i UTF-8 -d $base -a $base.words 2> /dev/null | grep -v 'International Ispell Version' | grep -v '^$' > $base-with-support.txt
 
 		diff $base-without-support.txt $base-with-support.txt > $base.diff
 
