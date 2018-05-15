78f0c11fc5f27ad81cd7298b140d3175f4188e6d
PanderMusubi <pander@users.sourceforge.net>
2018-05-15 11:53:25 +0200

    initial contribution tests special characters

---
+++ special-characters/README.md
@@ -0,0 +1,8 @@
+# Supporting special characters
+
+This directory contains tests to illustrate that certain special characters do need to be listed in `WORDCHARS` in an affix file.
+
+https://en.wikipedia.org/wiki/ISO/IEC_8859#Table
+
+
+Note that some files are symbolic links and other files are generated.
---
+++ special-characters/test.sh
@@ -0,0 +1,14 @@
+rm -f *.txt *.diff
+
+for file in *aff; do
+	base=`basename $file .aff`
+	if [ $file != none.aff ]; then
+
+		hunspell -i UTF-8 -d none -a $base 2> /dev/null | grep -v 'International Ispell Version' | grep -v '^$' > $base-without-support.txt
+
+		hunspell -i UTF-8 -d $base -a $base 2> /dev/null | grep -v 'International Ispell Version' | grep -v '^$' > $base-with-support.txt
+
+		diff $base-without-support.txt $base-with-support.txt > $base.diff
+
+	fi
+done
