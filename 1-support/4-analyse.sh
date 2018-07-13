4515f36a5d0620ed0c2365c31c3c0615d7aa8545
PanderMusubi <pander@users.sourceforge.net>
2018-07-13 18:45:41 +0200

    added analyses

---
+++ 1-support/4-analyse.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# description: analyses Hunspell language support
+# license: https://github.com/hunspell/nuspell/blob/master/LICENSES
+# author: Sander van Geloven
+
+if [ ! -d utf8 ]; then
+	echo 'ERROR: Run the script ./3-convert-and-report.sh first.'
+    exit 1
+fi
+
+cd utf8
+
+for i in *.dic; do
+    filename=`basename $i .dic`
+    echo $filename
+    ../../0-tools/histogram.py $i > $filename-historgram.md
+done
+
+cd ..
