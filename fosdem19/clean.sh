0518f7770cecc521a2f6e847afabbee6470eaaef
PanderMusubi <pander@users.sourceforge.net>
2019-02-01 15:48:52 +0100

    fosdem19

---
+++ fosdem19/clean.sh
@@ -0,0 +1,1 @@
+rm -f *.aux *.out *.log *.toc *.snm *.nav
---
+++ fosdem19/convert.sh
@@ -0,0 +1,6 @@
+cd images
+for i in *svg; do
+	inkscape -D -z --file=$i --export-pdf=`basename $i svg`pdf --export-latex
+done
+#mv -f *.pdf *.pdf_tex ..
+cd ..
