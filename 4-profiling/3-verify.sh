76f14887746c819e745cc3d41d1d17a5a8933b9c
PanderMusubi <pander@users.sourceforge.net>
2019-02-14 17:50:28 +0100

    minor

--- 4-profiling/3-verify.sh
+++ 4-profiling/3-verify.sh
@@ -5,6 +5,6 @@
 mkdir -p results/verify-nl
 cd results/verify-nl
 pwd
-perf record -e cycles -g --call-graph fp ../../../../nuspell/src/nuspell/verify -i UTF-8 -d ../../nl ../../verify-nl.txt -C ../../verify-nl.tsv 2> perf_stderr > perf_stdout
+perf record -e cycles:u --call-graph fp -- ../../../../nuspell/src/nuspell/verify -i UTF-8 -d ../../nl ../../verify-nl.txt -C ../../verify-nl.tsv 2> perf_stderr > perf_stdout
 #valgrind --tool=callgrind ../../../../nuspell/src/nuspell/verify -i UTF-8 -d ../../nl ../../verify-nl.txt -C ../../verify-nl.tsv 2> callgrind_stderr > callgrind_stdout
 cd ../..
