7065ee8a2c921da1a414d59dcbb3fda54d211672
PanderMusubi <pander@users.sourceforge.net>
2019-02-15 14:45:29 +0100

    minor

--- 4-profiling/3-verify.sh
+++ 4-profiling/3-verify.sh
@@ -2,9 +2,11 @@
 
 # license: https://github.com/hunspell/nuspell/blob/master/LICENSES
 
+BASE=`pwd`
 mkdir -p results/verify-nl
 cd results/verify-nl
 pwd
-perf record -e cycles:u --call-graph fp -- ../../../../nuspell/src/nuspell/verify -i UTF-8 -d ../../nl ../../verify-nl.txt -C ../../verify-nl.tsv 2> perf_stderr > perf_stdout
+#--call-graph dwarf -e cycles:u 
+perf record -a --call-graph fp ../../../../nuspell/src/nuspell/verify -i UTF-8 -d $BASE/nl $BASE/verify-nl.txt -C $BASE/verify-nl.tsv 2> perf_stderr > perf_stdout
 #valgrind --tool=callgrind ../../../../nuspell/src/nuspell/verify -i UTF-8 -d ../../nl ../../verify-nl.txt -C ../../verify-nl.tsv 2> callgrind_stderr > callgrind_stdout
 cd ../..
