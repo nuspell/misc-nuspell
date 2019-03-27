654b55a4b6574cb80e153bdccce03d84e231fe2c
PanderMusubi <pander@users.sourceforge.net>
2019-03-27 10:28:28 +0100

    added suggestion testing and small improvement deb package

---
+++ 3-checks/suggestions/README.md
@@ -0,0 +1,10 @@
+The lists with suggestions here are not to be redistributed and only for test
+purposes such as performance tests, verification tests and regression tests.
+
+The format of the TSV files is:
+1. incorrect word
+2. tab character
+3. correct suggestion
+
+The incorrect preferably does not contain space characters and is unique for
+the file.
---
+++ 3-checks/suggestions/check.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+for i in *.tsv; do
+	echo $i'\t'`wc -l $i|awk '{print $1}'`
+	awk -F '\t' '{print $1}' $i>/tmp/$i.1
+	if [ `wc -l $i|awk '{print $1}'` -ne `sort /tmp/$i.1|uniq|wc -l|awk '{print $1}'` ]; then
+		echo 'ERROR: Entries not unique for '$i
+		sort /tmp/$i.1|uniq -c|grep -v '^      1 '
+	fi
+#	awk -F '\t' '{print $2}' $i>/tmp/$i.2
+#FIXME	diff -y --suppress-common-lines /tmp/$i.1 /tmp/$i.2|wc -l
+done
