24b70f4155a695dbe7bc7653ea014b76693f879a
PanderMusubi <pander@users.sourceforge.net>
2020-04-06 13:56:54 +0200

    improved dictionary packages download

--- 1-support/1-download.sh
+++ 1-support/1-download.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env sh
 
-# description: downloads packages with Hunspell language support
-# license: https://github.com/hunspell/nuspell/blob/master/LICENSES
+# description: Downloads dictionary packages
+# license: https://github.com/nuspell/nuspell/blob/master/LICENSES
 # author: Sander van Geloven
-
-platform=`../0-tools/platform.sh`
 
 if [ -e packages ]; then
 	rm -f packages/*
@@ -13,114 +11,34 @@ else
 fi
 cd packages
 
-if [ $platform = linux ]; then
+# Only package names which start with myspell-, hunspell- or nuspell-,
+# except any dummy transitional packages or packages with tools.
+PACKAGES=$(apt-cache search 'myspell|hunspell|nuspell'|grep -E '^myspell-|^hunspell-|^nuspell-'|grep -iv transitional|grep -iv dummy|awk '{print $1}'|grep -v tools|tr '\n' ' ')
 
-    # Myspell available packages
-    MA=`apt-cache search myspell|grep ^myspell-|grep -iv transitional|grep -iv dummy|awk '{print $1}'|grep -v tools|tr '\n' ' '`
-    # Hunspell available packages
-    HA=`apt-cache search hunspell|grep ^hunspell|grep -iv transitional|grep -iv dummy|awk '{print $1}'|grep -v tools|tr '\n' ' '`
+SKIP='myspell-blah
+hunspell-blah'
 
-    # Myspell packages to download
-    # Omitting packages which have Hunspell equivalent
-    MD='myspell-eo
-        myspell-et
-        myspell-fa
-        myspell-fo
-        myspell-ga
-        myspell-gv
-        myspell-hy
-        myspell-nr
-        myspell-ns
-        myspell-sq
-        myspell-ss
-        myspell-st
-        myspell-tl
-        myspell-tn
-        myspell-ts
-        myspell-ve
-        myspell-xh
-        myspell-zu'
-    # Hunspell packages to download
-    HD=''
-
-    # Myspell packages to skip
-    MS=''
-    # Hunspell packages to skip
-    # Omitting meta packages and omitting conflicting packages
-    HS='hunspell-fr
-        hunspell-fr-classical
-        hunspell-fr-modern
-        hunspell-fr-revised
-	hunspell-fy
-	hunspell-tlh
-        hunspell-gl'
-
-    # Check is Myspell packages for download are available
-    for i in $MD; do
-        if [ `echo $MA|grep -c $i` -eq 0 ]; then
-            echo 'ERROR: Package '$i' to download is not available'
-            exit
-        fi
-    done
-
-    # Construct Myspell packages to skip
-    for i in $MA; do
-        if [ `echo $MD|grep -c $i` -eq 0 ]; then
-            echo 'Skipping package '$i'...'
-            MS=$MS' '$i
-        fi
-    done
-
-    # Check if skip package is still offered for download
-    for i in $HS; do
-        if [ `echo $HA|grep -c $i` -eq 0 ]; then
-            echo 'WARNING: Package '$i' to skip is not available'
-        else
-            echo 'Skipping package '$i'...'
-        fi
-    done
-
-    # Construct Hunspell packages to download
-    for i in $HA; do
-        if [ `echo $HS|grep -c $i` -eq 0 ]; then
-            HD=$HD' '$i
-        fi
-    done
-
-	# Double check for overlapping packages
-	for i in $MD; do
-		I=`echo $i|sed 's/myspell-//'`
-		for j in $HD; do
-			J=`echo $j|sed 's/hunspell-//'`
-			if [ $I = $J ]; then
-				echo 'ERROR: Overlapping packages '$i' and '$j
-				exit
-			fi
-		done
-	done
-
-	# Download packages
-	apt-get download $MD $HD
-	#TODO temporary workaround until package request has been completed
-	#TODO once fixed, update also variable HS above
-	# Frisian
-	wget https://raw.githubusercontent.com/PanderMusubi/frisian/master/packages/hunspell-fy_latest_all.deb
-	wget `echo https://raw.githubusercontent.com/PanderMusubi/frisian/master/packages/|awk -F 'hunspell-fy_latest_all.deb' '{print $1}'``cat hunspell-fy_latest_all.deb`
-	rm -f hunspell-fy_latest_all.deb
-	# Klingon
-	wget https://raw.githubusercontent.com/PanderMusubi/klingon/master/packages/hunspell-tlh_latest_all.deb
-	wget `echo https://raw.githubusercontent.com/PanderMusubi/klingon/master/packages/|awk -F 'hunspell-tlh_latest_all.deb' '{print $1}'``cat hunspell-tlh_latest_all.deb`
-	rm -f hunspell-tlh_latest_all.deb
-
-elif [ $platform = freebsd ]; then
-
-	sudo pkg fetch -o . -y `pkg search hunspell | grep ^[a-z][a-z]-hunspell | awk -F '-' '{print $1"-"$2}' | tr '\n' ' '`
-        if [ -d All ]; then
-		sudo mv All/* .
-		sudo rmdir All
-		sudo chown freebsd:freebsd *
+DOWNLOADS=''
+for i in $PACKAGES; do
+	if [ $(echo $SKIP|grep -c $i) -eq 0 ]; then
+		DOWNLOADS=$DOWNLOADS' '$i
+	else
+		echo 'Skipping package '$i'...'
 	fi
+done
 
-fi
+apt-get download $(echo $DOWNLOADS|sort)
+
+# Frisian
+wget --quiet https://raw.githubusercontent.com/PanderMusubi/frisian/master/packages/hunspell-fy_latest_all.deb
+wget $(echo https://raw.githubusercontent.com/PanderMusubi/frisian/master/packages/|awk -F 'hunspell-fy_latest_all.deb' '{print $1}')$(cat hunspell-fy_latest_all.deb)
+rm -f hunspell-fy_latest_all.deb
+
+# Klingon
+wget --quiet https://raw.githubusercontent.com/PanderMusubi/klingon/master/packages/hunspell-tlh_latest_all.deb
+wget $(echo https://raw.githubusercontent.com/PanderMusubi/klingon/master/packages/|awk -F 'hunspell-tlh_latest_all.deb' '{print $1}')$(cat hunspell-tlh_latest_all.deb)
+rm -f hunspell-tlh_latest_all.deb
+
+echo 'Downloaded in total '$(ls|wc -l)' packages'
 
 cd ..
