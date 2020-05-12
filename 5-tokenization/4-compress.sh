96d74a1523f1812d8f17064435e2f9a6bdf8c085
PanderMusubi <pander@users.sourceforge.net>
2020-05-12 10:12:19 +0200

    housekeeping

--- 5-tokenization/4-compress.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# description: compress Wikipedia extracts
-# license: https://github.com/hunspell/nuspell/blob/master/LICENSES
-# author: Sander van Geloven
-
-for i in *xml; do
-    bzip2 $i
-done
--- 5-tokenization/6-clean.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-# author: Sander van Geloven
-# license: https://github.com/hunspell/nuspell/blob/master/LICENSES
-# description: removed extracted articles from Wikipedia exports
-
-rm -rf *-latest-pages-articles-multistream
